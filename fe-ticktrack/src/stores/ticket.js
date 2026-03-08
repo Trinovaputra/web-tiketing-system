d96c06da62f9e4fbbbb4a04dba6c1f01c6b99e7e
trinova putra <trinovakerja@gmail.com>
2026-03-09 03:06:02 +0700

    first commit

---
+++ fe-ticktrack/src/stores/ticket.js
@@ -0,0 +1,75 @@
+import { defineStore } from "pinia";
+import { axiosInstance } from '@/plugins/axios';
+import { handleError } from "@/helpers/errorHelper";
+import router from "@/router";
+
+export const useTicketStore = defineStore("ticket", {
+    state: () => ({
+        tickets: [],
+        loading: false,
+        error: null,
+        success: null,
+    }),
+
+    actions: {
+        async fetchTickets(params) {
+            this.loading = true
+
+            try {
+                const response = await axiosInstance.get(`ticket`, { params })
+
+                this.tickets = response.data.data
+            } catch (error) {
+                this.error = handleError(error)
+            } finally {
+                this.loading = false
+            }
+        },
+
+        async fetchTicket(code) {
+            this.loading = true
+
+            try {
+                const response = await axiosInstance.get(`/ticket/${code}`)
+
+                return response.data.data
+            } catch (error) {
+                this.error = handleError(error)
+            } finally {
+                this.loading = false
+            }
+        },
+
+        async createTicket(payload) {
+            this.loading = true
+
+            try {
+                const response = await axiosInstance.post("/ticket", payload)
+
+                this.success = response.data.message
+
+                router.push({ name: 'app.dashboard' })
+            } catch (error) {
+                this.error = handleError(error)
+            } finally {
+                this.loading = false
+            }
+        },
+
+        async createTicketReply(code, payload) {
+            this.loading = true
+
+            try {
+                const response = await axiosInstance.post(`/ticket-reply/${code}`, payload)
+
+                this.success = response.data.message
+
+                return response.data.data
+            } catch (error) {
+                this.error = handleError(error)
+            } finally {
+                this.loading = false
+            }
+        },
+    }
+})
